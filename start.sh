beaf9bc82698faac4a01615a73d8c9110a9eebbd
Gautami Mamidala <gautamisaisatya@gmail.com>
2023-05-28 18:16:26 +0000

    Added docker commands in start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ uvicorn main:app --reload
 # uvicorn (web server): Starts our application
 # Reload it to update modified code
 
+# For docker
 docker-compose -f Docker-compose.yml build
 docker-compose -f Docker-compose.yml up
